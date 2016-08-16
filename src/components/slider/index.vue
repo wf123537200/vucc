b13bd25eb127ae47fef70379dcaccdb00f344717
wufeng <123537200@qq.com>
2016-08-16 14:59:15 +0800

    升级0.5.2 增加 固定点类型的slider

--- src/components/slider/index.vue
+++ src/components/slider/index.vue
@@ -7,11 +7,21 @@
    @param {String} type 当type为percent的时,将附加text = %
    @param {String | Number} value 当前滑动条的值
    @param {Boolean} isDisabled 当前滑动条不可滑动
-   @param {String} appendClass 自定义class
+   @param {Array} valueList 固定点的点阵渲染列表,如果传入,则按照固定点阵进行拖拽
+    ex:
+        [{
+            label: '0ms',
+            value: 0
+        }, {
+            label: '25ms',
+            value: 25
+        }]
+   @param {Object} appendClass 自定义Style对象
    @param {Object} appendClass 自定义Style对象
 -->
 <template>
-    <div :class="['tbd-slider', appendClass, {'tbd-slider-disabled': isDisabled}]" :style="appendStyle" @mousemove="mousemove($event)" @mousedown="mousedown($event)" @mouseup="mouseup($event)">
+    <div :class="['tbd-slider', appendClass, {'tbd-slider-disabled': isDisabled}]" :style="appendStyle"
+         @mousemove="mousemove($event)" @mousedown="mousedown($event)" @mouseup="mouseup($event)">
         <!-- 滑块 -->
         <div class="tbd-slider-handle" v-el:dot-btn :style="{'left' : percentValue + '%'}"></div>
 
@@ -19,13 +29,26 @@
              :style="{'width' : percentValue + '%'}"></div>
         <div class="tbd-slider-step" v-el:step></div>
 
-        <span class="tbd-slider-dot tbd-slider-dot-active" style="left:0%" v-el:dot-left
-              @click="dotClick(min)"></span>
-        <span class="tbd-slider-dot tbd-slider-dot-active" style="left:100%" v-el:dot-right
-              @click="dotClick(max)"></span>
+        <!-- 没有传入 valueList 只有两个端点 end -->
+        <span v-if="!valueList" class="tbd-slider-dot tbd-slider-dot-active"
+              style="left:0%" v-el:dot-left
+              @click="dotClick(min)">
+        </span>
+        <span v-if="!valueList" class="tbd-slider-dot tbd-slider-dot-active"
+              style="left:100%" v-el:dot-right
+              @click="dotClick(max)">
+        </span><!-- 没有传入 valueList 只有两个端点 end -->
 
-        <!-- 最小值滑块, 最大值滑块, 游标滑块下方为处理处理-->
-        <div class="tbd-slider-mark">
+        <!-- 传入valueList -->
+        <span v-for="it in valueList" class="tbd-slider-dot tbd-slider-dot-active"
+              :style="{'left': ($index / (valueList.length - 1) * 100) + '%'}" v-el:dot-left
+              @click="dotClick($index)">
+        </span>
+        <!-- 传入valueList end -->
+
+
+        <!-- valueList 未传入 最小值滑块, 最大值滑块, 游标滑块下方为处理处理 -->
+        <div v-if="!valueList" class="tbd-slider-mark">
             <span class="tbd-slider-mark-text tbd-slider-mark-text-active min">
                 {{parseInt(min) + text}}
             </span>
@@ -38,6 +61,14 @@
                 {{parseInt(max) + text}}
             </span>
         </div>
+        <!-- valueList 未传入 最小值滑块, 最大值滑块, 游标滑块下方为处理处理 end -->
+        <div v-for="it in valueList" class="tbd-slider-mark">
+            <span class="tbd-slider-mark-text tbd-slider-mark-text-active min"
+                  :style="{'left': ($index / (valueList.length - 1) * 100) + '%'}">
+                {{it.label}}
+            </span>
+        </div>
+
     </div>
 </template>
 
@@ -68,6 +99,9 @@
             isDisabled : {
                 type: Boolean,
                 default : false
+            },
+            valueList: {
+                type: Array
             }
         }),
 
@@ -81,17 +115,37 @@
 
         computed: {
             percentValue() {
+                let percent = 0;
+
                 if(this.value <= this.min) this.value = this.min;
 
-                let percent = (this.value - this.min) / (this.max - this.min);
-                return parseInt(percent * 100);
+                percent = (this.value - this.min) / (this.max - this.min);
+
+                // 固定点重置
+                if(this.valueList) {
+                    const curIndex = this.valueList.findIndex((it) => {return it.value == this.value});
+                    return curIndex / (this.valueList.length - 1) * 100;
+                } else {
+                    return parseInt(percent * 100);
+                }
+            },
+            valueListPoint() {
+                if(!this.valueList) return;
+                let res = [];
+                const len = this.valueList.length;
+
+                this.valueList.map((it, index) => {
+                    res.push(index / (len - 1));
+                });
+
+                return res
             }
         },
 
         methods: {
             // 点击端点,直接设置值
             dotClick(val) {
-                this.value = val;
+                this.value = this.valueList ? this.valueList[val].value : val;
             },
 
             //鼠标按下按键，更新鼠标点击值
@@ -110,12 +164,17 @@
                     let value = (this.max - this.min) * ((x - left) / (right - left));
                     this.value = parseInt(value) + parseInt(this.min);
                 }
+
+                // 固定点
+                if(this.valueList) {
+                    this.value = this.valueList[Math.round(this.value / 2000 / this.valueListPoint[1])].value;
+                }
             },
             //鼠标按键结束则停止更新值
             mouseup() {
                 if(this.isDisabled) return;
 
-                this.status = false
+                this.status = false;
             },
 
             //鼠标滑动过程动态更新值
@@ -140,6 +199,11 @@
                 if(x > right) {
                     this.value = this.max
                 }
+
+                // valueList 固定点
+                if(this.valueList) {
+                    this.value = this.valueList[Math.round(this.value / 2000 / this.valueListPoint[1])].value;
+                }
             },
 
             getRect(elements) {
@@ -154,8 +218,14 @@
                         right: rect.right - clientLeft // 距离右边的位置就是 距离左边的位置加上元素本身的宽度
                 })
             }
+        },
+        ready() {
+            if(!this.valueList) return;
+
+            this.min = this.valueList[0].value;
+            this.max = this.valueList[this.valueList.length - 1].value;
+        }
     }
-}
 </script>
 
 <style scoped>
--- src/demo.vue
+++ src/demo.vue
@@ -197,6 +197,10 @@
           <v-slider min="100" max="1000" :value.sync="sliderValue" :text="'px'"></v-slider>
         </div>
         <input type="text" style="margin-top:20px;" v-model="sliderValue"></input>
+        <div style="left: 10px; top: 200px; width: 400px;">
+          <v-slider :value.sync="sliderValue2" :text="'px'" :value-list="sliderValueList"></v-slider>
+        </div>
+        <input type="text" style="margin-top:20px;" v-model="sliderValue2"></input>
       </div>
 
       <h2>警告提示组件</h2>
@@ -738,7 +742,35 @@
         }],
 
         // 搜索框
-        search: ''
+        search: '',
+
+        // 固定点阵的滑动输入条
+        sliderValueList: [{
+          label: '0ms',
+          value: 0
+        }, {
+          label: '25ms',
+          value: 25
+        }, {
+          label: '50ms',
+          value: 50
+        }, {
+          label: '100ms',
+          value: 100
+        }, {
+          label: '200ms',
+          value: 200
+        }, {
+          label: '400ms',
+          value: 400
+        }, {
+          label: '1s',
+          value: 1000
+        }, {
+          label: '2s',
+          value: 2000
+        }],
+        sliderValue2: 50
       }
     },
 
