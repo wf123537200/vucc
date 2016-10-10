9d1e22e2ee8df881dad83a32ace8cc141661b1f0
wufeng <123537200@qq.com>
2016-10-10 17:15:50 +0800

    1.4.45 开关,增加text属性

--- src/components/switch/index.vue
+++ src/components/switch/index.vue
@@ -2,12 +2,16 @@
   switch 组件
   @param {Boolean} value 开关是否打开
   @param {String} [size=normal|large|small] 开关的大小
+  @param {String} text 开关内部需要展示的文字,样式需要自己添加
   @param {Boolean} isDisabled 开关是否禁用
   @param {Object} appendClass 自定义class
   @param {Object} appendStyle 自定义Style对象
 -->
 <template>
-  <div :style="appendStyle" :class="['vc-switch', appendClass, sizeClass, {'vc-switch-off': !value, 'vc-switch-disabled': isDisabled}]" @click="toggleSwitch"><i></i></div>
+  <div :style="appendStyle" :class="['vc-switch', appendClass, sizeClass, {'vc-switch-off': !value, 'vc-switch-disabled': isDisabled}]" @click="toggleSwitch">
+    {{text}}
+    <i></i>
+  </div>
 </template>
 
 <script>
@@ -26,6 +30,10 @@
       isDisabled: {
         type: Boolean,
         default: false
+      },
+      text: {
+        type: String,
+        default: ''
       }
     }),
 
