626bb5782940b887e083998fd7dabbd905de1ef4
wufeng <123537200@qq.com>
2016-12-14 17:51:26 +0800

    time-line 增加左侧属性~~

--- src/components/time-line/index.vue
+++ src/components/time-line/index.vue
@@ -4,14 +4,15 @@
    @param {Array} data 时间轴渲染数据
     ex:
         ['我们发布了alpha \n\r 再试试', '我们发布了beta']
+   @param {String} textType 定义说明是在时间轴左侧还是右侧
    @param {String} appendClass 自定义class
    @param {Object} appendStyle 自定义Style对象
 -->
 
 <template>
     <ul class="vc-timeline">
-        <li class="vc-timeline-item" v-for="it in data">
-            <i class="vc-timeline-item-dot"></i>
+        <li :class="['vc-timeline-item', {'vc-timeline-left': textType === 'left'}]" v-for="it in data">
+            <i :class="['vc-timeline-item-dot', {'vc-timeline-left-dot': textType === 'left'}]"></i>
             <div class="vc-timeline-item-content">
                 <p>
                     <span class="vc-timeline-item-info" v-html="it"></span>
@@ -28,6 +29,10 @@
         props: Object.assign({}, componentBaseParamConfig, {
             data: {
                 type: Array
+            },
+            textType: {
+                type: String,
+                default: 'right'
             }
         })
     }
