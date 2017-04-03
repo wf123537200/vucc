15950d7adfa5fed90d5a37ee22b00251b0f50aee
wufeng <123537200@qq.com>
2017-04-03 11:49:46 +0800

    更新热加载模式，方便调试

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -1,6 +1,7 @@
 var webpack = require('webpack');
 var merge = require('webpack-merge');
 var baseConfig = require('./webpack.base.conf');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // css不需要在打包时抽出和prd的区别
 baseConfig.module.loaders.push({
@@ -19,5 +20,17 @@ module.exports = merge(baseConfig, {
         path: "./dist",
         publicPath: "dist",
         filename: "[name].js"
-    }
+    },
+    plugins: [
+        // 生成 html 文件
+        new HtmlWebpackPlugin({
+            template: './test/index.html', // html模版
+            hash: false, // 文件名是否 hash
+            filename: 'index.html', // 文件名
+            inject: 'body', // 静态文件注入的位置
+            minify: {
+                collapseWhitespace: true // 压缩空白
+            }
+        })
+    ]
 });
